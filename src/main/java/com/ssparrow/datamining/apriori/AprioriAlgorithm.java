25c4efbf564d4ecfc8956bf1b7c94b9d59fd9651
Fei Gao <eaglelion8038@hotmail.com>
2012-09-05 23:16:10 -0700

    define packages and classes

---
+++ src/main/java/com/ssparrow/datamining/apriori/AprioriAlgorithm.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.ssparrow.datamining.apriori;
+
+/**
+ * @author Gao, Fei
+ *
+ */
+public class AprioriAlgorithm {
+
+}
---
+++ src/test/java/com/ssparrow/datamining/apriori/AprioriAlgorithmtest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.ssparrow.datamining.apriori;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Gao, Fei
+ *
+ */
+public class AprioriAlgorithmtest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
