ccb28e84afefd8e2d6014c6c0310f262d048337f
Fei Gao <eaglelion8038@hotmail.com>
2012-09-06 00:46:52 -0700

    implemented Apriori Algorithm according to pseudo code on wikipedia

--- src/main/java/com/ssparrow/datamining/apriori/AprioriAlgorithm.java
+++ src/main/java/com/ssparrow/datamining/apriori/AprioriAlgorithm.java
@@ -3,10 +3,97 @@
  */
 package com.ssparrow.datamining.apriori;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * @author Gao, Fei
  *
  */
 public class AprioriAlgorithm {
-
+	
+	/**
+	 * implementation according to pseudo code of apriori algorithm on wikipedia
+	 * 
+	 * http://en.wikipedia.org/wiki/Apriori_algorithm
+	 * 
+	 * @param transactions
+	 * @param threshold
+	 * @return
+	 */
+	public static Map<Set<String>, Integer> findPatterns(List<List<String>> transactions, int threshold){
+		
+		//count the occurrence of all single items
+		Map<Set<String>, Integer> countMap=new LinkedHashMap<Set<String>, Integer>();
+		for(List<String> transaction:transactions){
+			for(String item: transaction){
+				Set<String> itemSet=new TreeSet<String>();
+				itemSet.add(item);
+				
+				int count=countMap.get(itemSet)==null?1:countMap.get(itemSet)+1;
+				countMap.put(itemSet, count);
+			}
+		}
+		
+		//find the single items that are candidates for memebers of possible pattern set
+		Set<String> singleCandidates=new TreeSet<String>();
+		Map<Set<String>, Integer> candidates=new LinkedHashMap<Set<String>, Integer>();
+		for(Set<String> itemSet:countMap.keySet()){
+			int count=countMap.get(itemSet)==null?0:countMap.get(itemSet);
+			
+			if(count>=threshold){
+				candidates.put(itemSet,count);
+				singleCandidates.addAll(itemSet);
+			}
+		}
+		
+		Map<Set<String>, Integer> patterns = new LinkedHashMap<Set<String>, Integer>();
+		while(!candidates.isEmpty()){
+			//generate the next level item sets by adding possible single item to current level set
+			List<Set<String>> nextLevelItemSets=new ArrayList<Set<String>>();
+			for(Set<String> itemSet:candidates.keySet()){
+				for(String item:singleCandidates){
+					if(!itemSet.contains(item)){
+						Set<String> newItemSet=new TreeSet<String>(itemSet);
+						newItemSet.add(item);
+						
+						if(!nextLevelItemSets.contains(newItemSet)){
+							nextLevelItemSets.add(newItemSet);
+						}
+					}
+				}
+			}
+			
+			//count the support of each item set
+			countMap=new LinkedHashMap<Set<String>, Integer>();
+			for(Set<String> itemSet:nextLevelItemSets){
+				for(List<String> transaction:transactions){
+					if(transaction.containsAll(itemSet)){
+						int count=countMap.get(itemSet)==null?1:countMap.get(itemSet)+1;
+						countMap.put(itemSet, count);
+					}
+				}
+			}	
+			
+			//find the patterns for next level(start with level 2)
+			candidates=new LinkedHashMap<Set<String>, Integer>();
+			for(Set<String> itemSet:countMap.keySet()){
+				int count=countMap.get(itemSet)==null?0:countMap.get(itemSet);
+				
+				if(count>=threshold){
+					candidates.put(itemSet,count);
+					patterns.put(itemSet,count);
+				}
+			}
+		}
+		
+		return patterns;
+	}
+	
+	
 }
--- src/test/java/com/ssparrow/datamining/apriori/AprioriAlgorithmTest.java
+++ src/test/java/com/ssparrow/datamining/apriori/AprioriAlgorithmTest.java
@@ -5,6 +5,12 @@ package com.ssparrow.datamining.apriori;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.Test;
 
 /**
@@ -13,9 +19,88 @@ import org.junit.Test;
  */
 public class AprioriAlgorithmTest {
 
+	/**
+	 * test using the example on wikipedia
+	 *   {1,2,3,4}, {1,2}, {2,3,4}, {2,3}, {1,2,4}, {3,4}, and {2,4}
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFindPatterns() {
+		List<List<String>> transactions = new ArrayList<List<String>>();
+		
+		//{1,2,3,4}
+		List<String> transaction=new ArrayList<String>();
+		transaction.add("1");
+		transaction.add("2");
+		transaction.add("3");
+		transaction.add("4");
+		transactions.add(transaction);
+		
+		//{1,2}
+		transaction=new ArrayList<String>();
+		transaction.add("1");
+		transaction.add("2");
+		transactions.add(transaction);
+		
+		//{2,3,4}
+		transaction=new ArrayList<String>();
+		transaction.add("2");
+		transaction.add("3");
+		transaction.add("4");
+		transactions.add(transaction);
+
+		//{2,3}
+		transaction=new ArrayList<String>();
+		transaction.add("2");
+		transaction.add("3");
+		transactions.add(transaction);
+
+		//{1,2,4}
+		transaction=new ArrayList<String>();
+		transaction.add("1");
+		transaction.add("2");
+		transaction.add("4");
+		transactions.add(transaction);
+
+		//{3,4}
+		transaction=new ArrayList<String>();
+		transaction.add("3");
+		transaction.add("4");
+		transactions.add(transaction);
+
+		//{2,4}
+		transaction=new ArrayList<String>();
+		transaction.add("2");
+		transaction.add("4");
+		transactions.add(transaction);
+		
+		Map<Set<String>, Integer> patterns = AprioriAlgorithm.findPatterns(transactions, 3);
+		assertEquals(4, patterns.size());
+		
+		Iterator<Set<String>> iterator = patterns.keySet().iterator();
+		
+		Set<String> itemSet = iterator.next();
+		Iterator<String> itemIterator = itemSet.iterator();
+		assertEquals("1", itemIterator.next());
+		assertEquals("2", itemIterator.next());
+		assertEquals(Integer.valueOf(3), patterns.get(itemSet));
+		
+		itemSet = iterator.next();
+		itemIterator = itemSet.iterator();
+		assertEquals("2", itemIterator.next());
+		assertEquals("3", itemIterator.next());
+		assertEquals(Integer.valueOf(3), patterns.get(itemSet));
+		
+		itemSet = iterator.next();
+		itemIterator = itemSet.iterator();
+		assertEquals("2", itemIterator.next());
+		assertEquals("4", itemIterator.next());
+		assertEquals(Integer.valueOf(4), patterns.get(itemSet));
+		
+		itemSet = iterator.next();
+		itemIterator = itemSet.iterator();
+		assertEquals("3", itemIterator.next());
+		assertEquals("4", itemIterator.next());
+		assertEquals(Integer.valueOf(3), patterns.get(itemSet));
 	}
 
 }
