8c1e6ce8d19afd7e495e967f7d2b6f4c3ef5c0f6
Fei Gao <eaglelion8038@hotmail.com>
2012-09-24 10:43:14 -0700

    add script to run the prototype

---
+++ DistributedPatternDiscoveryMaster.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp DistributedPatternDiscovery.jar com.ssparrow.datamining.association.fpgrowth.distributed.DistributedPatternDiscoveryMaster Yoda transactionlist1.txt 5
---
+++ DistributedPatternDiscoveryWorker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp DistributedPatternDiscovery.jar com.ssparrow.datamining.association.fpgrowth.distributed.DistributedPatternDiscoveryNode ObiWan transactionlist2.txt
